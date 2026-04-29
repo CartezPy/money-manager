f1a5485eb3308c769d775858893cb42fc1bfdd0e
CartezPy <cartez97@ukr.net>
2026-04-30 01:20:44 +0300

    Add main program

---
+++ main.py
@@ -0,0 +1,20 @@
+# Money Manager CLI (simple version)
+
+# input data
+money = int(input("Enter your money: "))
+salary = int(input("Enter your salary: "))
+expenses = int(input("Enter your expenses: "))
+days = int(input("Enter days: "))
+
+# calculations
+total_money = money + salary - expenses
+money_per_day = total_money // days
+rest_money = total_money % days
+
+# output
+print("\n--- RESULT ---")
+print(f"Total money: {total_money}")
+print(f"Money per day: {money_per_day}")
+print(f"Left: {rest_money}")
+
+
